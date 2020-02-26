1eb4384c31b62c058216efb23edd621be0caa123
Igor Walace Cuevas <igor.w.cuevas@gmail.com>
2020-02-26 13:36:00 -0300

    Finalização Queries

---
+++ Todo.Domain/Queries/TodoQueries.cs
@@ -0,0 +1,25 @@
+using System;
+using System.Linq.Expressions;
+using Todo.Domain.Entities;
+
+namespace Todo.Domain.Queries
+{
+    public static class TodoQueries
+    {
+        public static Expression<Func<TodoItem, bool>> GetAll(string refUser)
+        {
+            return x => x.RefUser == refUser;
+        }
+        public static Expression<Func<TodoItem, bool>> GetAllUndone(string refUser)
+        {
+            return x => x.RefUser == refUser && !x.Done;
+        }
+        public static Expression<Func<TodoItem,bool>> GetPeriod(string refUser, DateTime date, bool done)
+        {
+            return x =>
+                    x.RefUser == refUser &&
+                    x.Done == done &&
+                    x.Date.Date == date.Date;
+        }
+    }
+}
